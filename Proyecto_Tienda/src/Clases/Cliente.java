1ff0efddaa8f28be12f09970078a96c8858b32f0
julen <santinazcona@opendeusto.es>
2018-10-22 09:35:21 +0200

    Clase Cliente y Compra

--- Proyecto_Tienda/src/Clases/Cliente.java
+++ Proyecto_Tienda/src/Clases/Cliente.java
@@ -1,7 +1,58 @@
 package Clases;
 
 public class Cliente {
+	private String Nombre;
+	private String DNI;
+	private String TarjetaCredito;
+	private String Direccion;
+	private String Email;
+	private String Contrasenia;
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getDNI() {
+		return DNI;
+	}
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+	public String getTarjetaCredito() {
+		return TarjetaCredito;
+	}
+	public void setTarjetaCredito(String tarjetaCredito) {
+		TarjetaCredito = tarjetaCredito;
+	}
+	public String getDireccion() {
+		return Direccion;
+	}
+	public void setDireccion(String direccion) {
+		Direccion = direccion;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getContrasenia() {
+		return Contrasenia;
+	}
+	public void setContrasenia(String contrasenia) {
+		Contrasenia = contrasenia;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cliente [Nombre=" + Nombre + ", DNI=" + DNI
+				+ ", TarjetaCredito=" + TarjetaCredito + ", Direccion="
+				+ Direccion + ", Email=" + Email + ", Contrasenia="
+				+ Contrasenia + "]";
+	}
 
+	
 }
 
 
