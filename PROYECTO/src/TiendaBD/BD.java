7ff55cfe028e48d7295a4e39750b19ffe9d9efc1
julen <santinazcona@opendeusto.es>
2018-11-23 11:38:39 +0100

    BD modificada

--- PROYECTO/src/TiendaBD/BD.java
+++ PROYECTO/src/TiendaBD/BD.java
@@ -1,13 +1,17 @@
+package TiendaBD;
 
-	package TiendaBD;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.Vector;
+import java.awt.*;
+import java.awt.event.*;
 
-	import java.sql.*;
-	import java.util.Vector;
-	import java.awt.*;
-	import java.awt.event.*;
-	import javax.swing.*;
-	import javax.swing.table.DefaultTableModel;
-	import Clases.Usuario;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+import Clases.Articulo;
+import Clases.Usuario;
+import Clases.Compra;
 
 	public class BD {
 		
@@ -15,48 +19,91 @@
 			private static Statement st;
 			private static ResultSet rs;
 
-//					try {
-//						com = "create table Usuario( nick STRING, pass STRING )";
-//						s.executeUpdate( com );
-//					} catch (SQLException e) {} 
-//					com = "select * from Cliente where nick = 'Paco'";
-//					rs = s.executeQuery( com );
-//					if (!rs.next()) {
-//						com = "insert into Cliente ( nick, Contrasenia ) values ('Paco', 'vivaelvino')";
-//						s.executeUpdate( com );
-//					}
-//					anyadirUsuarios();
-//
-//				} catch (SQLException|ClassNotFoundException e) {
-//					System.out.println( "Último comando: " + com );
-//					 e.printStackTrace();
-//				}
-//			}
-			
-			public static void main(String[] args) throws ClassNotFoundException {
-				String com = "";
+			public void conectar(){
 				try {
 					Class.forName( "org.sqlite.JDBC" );
 					con = DriverManager.getConnection( "jdbc:sqlite:BD" );
 					st = con.createStatement();
-					try {
-						com = "create table usuarios( Nick STRING, DNI STRING, TarjetaCredito STRING, Direccion STRING, Email STRING, Contrasenia INT )";
-						st.executeUpdate( com );
-					} catch (SQLException e) {}
-					com = "select * from usuarios where Nick = 'pepe'";
-					rs = st.executeQuery( com );
-					if (!rs.next()) {
-						com = "insert into usuarios ( Nick, DNI, TarjetaCredito, Direccion, Email,  Contrasenia ) values ('admin', 'admin', 'admin', 'admin', 'admin', '4566756')";
-						st.executeUpdate( com );
-					}
-				}catch (SQLException|ClassNotFoundException e) {
-					System.out.println( "Último comando: " + com );
+				}catch(Exception e){
+					System.out.println("No se ha conectado");
 					e.printStackTrace();
+				}
 			}
+
+//			public void insertarUsuario(String nombre, String apellidos,String direccion, String poblacion,int CP, String CodigoUsuario, String passwordUsuario,int CuentaBancaria){
+//				String com = "insert into usuarios ( nombre, apellidos, direccion, poblacion, CP,  CodigoUsuario,passwordUsuario,CuentaBancaria ) values ('"+nombre+"', '"+apellidos+"', '"+direccion+"', '"+poblacion+"', '"+CP+"', '"+CodigoUsuario+"','"+passwordUsuario+"','"+CuentaBancaria+"')";
+//				try {
+//						st.executeUpdate(com);
+//				
+//				}catch (SQLException e) {
+//					e.printStackTrace();
+//				}
+//	}
+	
+			public void insertarArticulo(int cod, String nombre, String tipo, String precio, int cantidad){
+				String com = "insert into articulo ( cod, nombre, tipo, precio, cantidad ) values ('"+cod+"', '"+nombre+"', '"+tipo+"', '"+precio+"', '"+cantidad+"')";
+				try {
+						st.executeUpdate(com);
+						
+				}catch (SQLException e) {
+					e.printStackTrace();
+				}
+				
 			}
+			
+		public void insertarUsuario(LinkedList<Usuario> usuario){
+		
+			for(int i=0;i<usuario.size();i++){
+				Usuario u = usuario.get(i);
+				String com = "INSERT INTO usuarios( nombre, apellidos, direccion, poblacion, CP, CodigoUsuario,passwordUsuario,CuentaBancaria ) values ('"+u.getNombre()+"', '"+u.getApellidos()+"', '"+u.getDireccion()+"', '"+u.getPoblacion()+"', '"+u.getCodigoPostal()+"', '"+u.getCodigoUsuario()+"','"+u.getPasswordUsuario()+"','"+u.getCuentaBancaria()+"')";
+				try {
+					st.executeUpdate(com);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		public void modificarUnidadesArticulo(int codigo, int unidades){
+			String com= "UPDATE articulo SET unidades=unidades-"+unidades+" WHERE codigo="+codigo;
+			try {
+				st.executeUpdate(com);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		public void modificarContraseñaUsuario(String CodigoUsuario, String passwordUsuario){
+			String com= "UPDATE usuarios SET passwordUsuario="+passwordUsuario+" WHERE CodigoUsuario="+CodigoUsuario;
+			try {
+				st.executeUpdate(com);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+			
+//			public static void main(String[] args) throws ClassNotFoundException {
+//				String com = "";
+//				try {
+//					Class.forName( "org.sqlite.JDBC" );
+//					con = DriverManager.getConnection( "jdbc:sqlite:BD" );
+//					st = con.createStatement();
+//					try {
+//						com = "create table usuarios( Nick STRING, DNI STRING, TarjetaCredito STRING, Direccion STRING, Email STRING, Contrasenia INT )";
+//						st.executeUpdate( com );
+//					} catch (SQLException e) {}
+//					com = "select * from usuarios where Nick = 'pepe'";
+//					rs = st.executeQuery( com );
+//					if (!rs.next()) {
+//						com = "insert into usuarios ( Nick, DNI, TarjetaCredito, Direccion, Email,  Contrasenia ) values ('admin', 'admin', 'admin', 'admin', 'admin', '4566756')";
+//						st.executeUpdate( com );
+//					}
+//				}catch (SQLException|ClassNotFoundException e) {
+//					System.out.println( "Último comando: " + com );
+//					e.printStackTrace();
+//			}
+//			}
 	}
 
 				
 			
-
-
