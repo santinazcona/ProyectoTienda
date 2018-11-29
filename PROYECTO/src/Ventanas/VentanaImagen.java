e2d91c15f26a61cb2f3bba4a3533f50f41ef39a8
julen <santinazcona@opendeusto.es>
2018-11-29 11:09:12 +0100

    Ventana para poner la imagen de fondo.

---
+++ PROYECTO/src/Ventanas/VentanaImagen.java
@@ -0,0 +1,19 @@
+package Ventanas;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+public class VentanaImagen extends javax.swing.JPanel {
+	public VentanaImagen(){
+		this.setSize(750, 387);
+	}
+	@Override
+	public void paintComponent (Graphics g){
+		Dimension tamanio = getSize();
+		ImageIcon imagenFondo = new ImageIcon(getClass().getResource("/imagenes/TiendaInformatica.jpg"));
+		g.drawImage(imagenFondo.getImage(),0,0,tamanio.width, tamanio.height, null);
+		setOpaque(false);
+		super.paintComponent(g);
+	}
+}
