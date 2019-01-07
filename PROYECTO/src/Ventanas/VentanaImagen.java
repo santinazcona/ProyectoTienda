6b5c841ebd9b0f1bb4458e7fbe754c95339eeaf9
julen <santinazcona@opendeusto.es>
2019-01-07 18:26:12 +0100

    Ventana para montrar la imagen de fondo de las ventanas

--- PROYECTO/src/Ventanas/VentanaImagen.java
+++ PROYECTO/src/Ventanas/VentanaImagen.java
@@ -2,19 +2,32 @@ package Ventanas;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
-import javax.swing.ImageIcon;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
-public class VentanaImagen extends javax.swing.JPanel {
-	public VentanaImagen(){
-		this.setSize(750, 387);
-	}
-	
-	
-	public void paintComponent (Graphics g){
-		Dimension tamanio = getSize();
-		ImageIcon imagenFondo = new ImageIcon(getClass().getResource("/imagenes/TiendaInformatica.jpg"));
-		g.drawImage(imagenFondo.getImage(),0,0,tamanio.width, tamanio.height, null);
-		setOpaque(false);
-		super.paintComponent(g);
-	}
+public class VentanaImagen extends JPanel {
+	private static final String DIR_PROYECTO = System.getProperty("user.dir");
+	private static final long serialVersionUID = -6113160396371828965L;
+	private BufferedImage image;
+
+    public VentanaImagen() {
+        try {
+            image = ImageIO.read(new FileInputStream(DIR_PROYECTO + "/imagenes/TiendaInformatica.jpg"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }  
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(500, 500);
+    }
 }
